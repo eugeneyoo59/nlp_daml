058c2f78b98359bb1c88bd9c952cb35630da2f05
ruben-marcus <rubenmarcus4@gmail.com>
2025-10-26 11:45:09 -0400

    added error handling

--- src/utils/tf_idf.py
+++ src/utils/tf_idf.py
@@ -6,8 +6,11 @@ def tf(term, doc):
     @param doc: list of words
     @return: term frequency (relative frequency) of term
     '''
+
+    if len(doc) = 0:
+        raise ValueError('cannot divide by zero')
     
-    return doc.count(term)/ len(doc)
+    return doc.count(term) / len(doc)
 
 
 def idf(term, docs):
@@ -18,6 +21,12 @@ def idf(term, docs):
     '''
     
     num_docs_with_term = len([doc for doc in docs if term in doc])
+
+    if num_docs_with_term == 0:
+        raise ValueError('cannot divide by zero')
+
+    if len(docs) == 0:
+        raise ValueError('cannot take log of zero')
 
     return math.log(len(docs) / num_docs_with_term)
 
