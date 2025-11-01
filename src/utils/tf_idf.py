c3b74763799350e057cd1187d3264be693618368
Ruben Marcus <rubenmarcus4@gmail.com>
2025-11-01 15:02:22 -0400

    added dependencies to environment.yml, changed tf_idf to match data_processing format

--- src/utils/tf_idf.py
+++ src/utils/tf_idf.py
@@ -3,9 +3,11 @@ import math
 def tf(term, doc):
     '''
     @param term: word to look for
-    @param doc: list of words
+    @param doc: processed text of words
     @return: term frequency (relative frequency) of term
     '''
+
+    doc = doc.split()
 
     if len(doc) == 0:
         raise ValueError('cannot divide by zero')
@@ -16,11 +18,13 @@ def tf(term, doc):
 def idf(term, docs):
     '''
     @param term: word to look for
-    @param docs: list of docs, each doc is a list of words
+    @param docs: list of docs, each doc is a processed text of words
     @return: inverse document frequency of term
     '''
+
+    docs = [d.split() for d in docs]
     
-    num_docs_with_term = len([doc for doc in docs if term in doc])
+    num_docs_with_term = len([d for d in docs if term in d])
 
     if num_docs_with_term == 0:
         raise ValueError('cannot divide by zero')
@@ -35,19 +39,25 @@ def tf_idf(term, doc, docs):
     '''
     @param term: word to look for
     @param doc: specific document to calculate tf for
-    @param docs: list of docs, each doc is a list of words
+    @param docs: list of docs, each doc is a processed text of words
     @return: tf * idf of word
     '''
+
+    doc = doc.split()
+    docs = [d.split() for d in docs]
 
     return tf(term, doc) * idf(term, docs)
 
 
 def vectorize(doc, docs):
     '''
-    @param doc: specific doc to vectorize, a list of words
-    @param docs: list of docs, each doc is a list of words
+    @param doc: specific doc to vectorize, a processed text of words
+    @param docs: list of docs, each doc is a processed text of words
     @return: dictionary mapping each unique word in docs to its tf-idf value in doc
     '''
+
+    doc = doc
+    docs = [d.split() for d in docs]
 
     unique_terms = sorted(set(term for d in docs for term in d))
 
