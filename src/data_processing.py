f27137ded2a59bdc13fdae80fc6b006d94081a43
Ruben Marcus <rubenmarcus4@gmail.com>
2025-11-01 15:42:16 -0400

    rearranged files (added preprocessing.py)

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,34 +1,10 @@
 import pandas as pd
-import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('wordnet')
+from utils.preprocessing import preprocess
 
 df = pd.read_csv("data/WELFake_Dataset.csv")
 df = df.rename(columns={"Unnamed: 0": "id",  "text": "tokens"})
 
-stop_words = set(stopwords.words('english'))
-stemmer = PorterStemmer()
-lemmatizer = WordNetLemmatizer()
-
-def preprocess_text(text):
-    #to lower case
-    text = text.lower() 
-
-    #remove punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    
-    #stopwords, stemming, lemmatization
-    words = nltk.word_tokenize(text)
-    words = [w for w in words if w not in stop_words]
-    #words = [stemmer.stem(w) for w in words]
-    words = [lemmatizer.lemmatize(w) for w in words]
-    return ' '.join(words)
+# print(preprocess(df.at[0, 'tokens']))
 
 #print(df.info())
 
---
+++ src/utils/preprocessing.py
@@ -0,0 +1,27 @@
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+def preprocess(text):
+    #to lower case
+    text = text.lower() 
+
+    #remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    #stopwords, stemming, lemmatization
+    words = nltk.word_tokenize(text)
+    words = [w for w in words if w not in stop_words]
+    #words = [stemmer.stem(w) for w in words]
+    words = [lemmatizer.lemmatize(w) for w in words]
+    return ' '.join(words)
