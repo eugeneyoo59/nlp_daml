2bef446e4f5490e09a5e919c2acb2bfa0deb7157
Matthew Del Preto <mdd74@duke.edu>
2025-11-14 13:35:03 -0500

    added baseline class.

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,5 +1,11 @@
 from preprocessing import clean_text
 from td_idf import TFIDFVectorizer
 
-class FeatureEngineer:
-    
+class TFIDFFeatureEngineer:
+    def init(self, max_features=5000):
+        self.max_features = max_features
+        self.vectorizer = TFIDFVectorizer(max_features=max_features)
+    def clean:
+    def fit:
+    def transform:
+    def fit_transform:
