f77c721bd20c33770051d5564d3107bd0d98a922
Matthew Del Preto <mdd74@duke.edu>
2025-11-14 13:41:16 -0500

    added init and clean functions

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -5,7 +5,8 @@ class TFIDFFeatureEngineer:
     def init(self, max_features=5000):
         self.max_features = max_features
         self.vectorizer = TFIDFVectorizer(max_features=max_features)
-    def clean:
+    def clean(self, texts):
+        return [clean_text(t) for t in texts]
     def fit:
     def transform:
     def fit_transform:
