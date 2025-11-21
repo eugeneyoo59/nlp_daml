fc4c1b05f07603e9991b720f2e52df8159a30844
AKarenin <alexdhkil@gmail.com>
2025-11-21 13:08:46 -0500

    finished feature eng

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,6 +1,19 @@
+from utils.tf_idf import tf_idf_vectorize
 from utils.preprocessing import preprocess
-import utils.tf_idf
 
-
-class FeatureEngineer:
+class TFIDFFeatureEngineer:
+    def __init__(self, max_features=5000):
+        self.max_features = max_features
+        self.vectorizer = tf_idf_vectorize(max_features=max_features)
+    def clean(self, texts):
+        return [preprocess(t) for t in texts]
+    def fit(self, texts):
+        cleaned = self.clean(texts)
+        return self.vectorizer.fit(cleaned)
+    def transform(self, texts):
+        cleaned = self.clean(texts)
+        return self.vectorizer.transform(cleaned)
+    def fit_transform(self, texts):
+        cleaned = self.clean(texts)
+        return self.vectorizer.fit_transform(cleaned)
     
